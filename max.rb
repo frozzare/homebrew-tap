904a4067e3af7626f876709fecd7b5b74f5b0faf
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-13 17:33:03 +0200

    max version v1.1.6

--- max.rb
+++ max.rb
@@ -1,9 +1,9 @@
 class Max < Formula
   desc "Max is a YAML-based task runner"
   homepage "https://github.com/frozzare/max"
-  url "https://github.com/frozzare/max/releases/download/v1.1.5/max_1.1.5_darwin_amd64.tar.gz"
-  version "1.1.5"
-  sha256 "62e9fc62a99d90c889642c864789b83e0f01ea3ef1a1b877b9021ef33695248a"
+  url "https://github.com/frozzare/max/releases/download/v1.1.6/max_1.1.6_darwin_amd64.tar.gz"
+  version "1.1.6"
+  sha256 "dcbf5e2508a229fa24c9944df07929f006517b2375e28a319220b41ba14c37e8"
 
   def install
     bin.install "max"

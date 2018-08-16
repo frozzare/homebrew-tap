e34b62a6f8323f9437ce0fec1eba55fb7ad0991f
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-16 09:10:02 +0200

    Brew formula update for create-project version v1.0.1

--- create-project.rb
+++ create-project.rb
@@ -1,9 +1,9 @@
 class CreateProject < Formula
   desc "Create project is a command line tool for create new project from a boilerplate."
   homepage "https://github.com/frozzare/create-project"
-  url "https://github.com/frozzare/create-project/releases/download/v1.0.0/create-project_1.0.0_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "5ed868f870fac9bb52fa717543f1356bf14067e16c62da783a1f88d55da96093"
+  url "https://github.com/frozzare/create-project/releases/download/v1.0.1/create-project_1.0.1_darwin_amd64.tar.gz"
+  version "1.0.1"
+  sha256 "d877cbc0f713af5c553598b5ab611374673762313e407b97ec5695959922e61c"
 
   def install
     bin.install "create-project"
